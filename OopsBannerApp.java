d3e2e45375720a240356f74c1f87628e599b55f0
amitkr069 <aman3133@7-11.com>
2026-02-18 16:58:30 +0530

    Coded to print OOPS Banner using Methods

--- OopsBannerApp.java
+++ OopsBannerApp.java
@@ -1,30 +1,62 @@
 /**
 @Author Amit
 
-@Version 5.0
+@Version 6.0
 
 */
 
+// this is for printing O pattern using method
 class OopsBannerApp{
+	public static String[] getOPattern(){
+		return new String[]{
+			"    **    ",
+			"  **  **  ",
+			" **    ** ",
+			" **    ** ",
+			" **    ** ",
+			"  **  **  ",
+			"    **    "
+		};
+		
+	}
+	
+	public static String[] getPPattern(){
+		return new String[]{
+			" ******* ",
+			" **    * ",
+			" **    * ",
+			" ******* ",
+			" **      ",
+			" **      ",
+			" **      "
+		};
+	
+	}
+	
+	public static String[] getSPattern(){
+		return new String[]{
+			"   ****  ",
+			" **      ",
+			"**       ",
+			"  *****   ",
+			"      **  ",
+			" **   **  ",
+			"   ****   "
+		};
+	}
 	public static void main(String[] args) {
         // System.out.println("Try programiz.pro");
         
-        String line[] = {
-			String.join(" ","    **    ","    **    "," ******* ","   ****  "),
-			String.join(" ","  **  **  ","  **  **  "," **    * "," **      "),
-			String.join(" "," **    ** "," **    ** "," **    * ","**       "),
-			String.join(" "," **    ** "," **    ** "," ******* ","  *****   "),
-			String.join(" "," **    ** "," **    ** "," **      ","      **  "),
-			String.join(" ","  **  **  ","  **  **  "," **      "," **   **  "),
-			String.join(" ","    **    ","    **    "," **      ","   ****   ")
-			
-		};
         
         
         
-        for(String s: line){
-            System.out.println(s);
-        }
+        String[] OPattern = getOPattern();
+		String[] PPattern = getPPattern();
+		String[] SPattern = getSPattern();
+		
+		for(int i = 0; i<OPattern.length; i++){
+			System.out.println(OPattern[i] + " " + OPattern[i] + " " + PPattern[i] + " " + SPattern[i]);
+		}
         
         // System.out.println(line[0]);
         
