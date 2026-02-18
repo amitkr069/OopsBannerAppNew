ff011fe5dfbd533936188f38844d1932385a61d9
amitkr069 <aman3133@7-11.com>
2026-02-18 16:03:14 +0530

    Coded to Render OOPS Banner Using Array Inline

--- OopsBannerApp.java
+++ OopsBannerApp.java
@@ -1,7 +1,7 @@
 /**
 @Author Amit
 
-@Version 4.0
+@Version 5.0
 
 */
 
@@ -9,21 +9,17 @@ class OopsBannerApp{
 	public static void main(String[] args) {
         // System.out.println("Try programiz.pro");
         
-        String line[] = new String[7];
+        String line[] = {
+			String.join(" ","    **    ","    **    "," ******* ","   ****  "),
+			String.join(" ","  **  **  ","  **  **  "," **    * "," **      "),
+			String.join(" "," **    ** "," **    ** "," **    * ","**       "),
+			String.join(" "," **    ** "," **    ** "," ******* ","  *****   "),
+			String.join(" "," **    ** "," **    ** "," **      ","      **  "),
+			String.join(" ","  **  **  ","  **  **  "," **      "," **   **  "),
+			String.join(" ","    **    ","    **    "," **      ","   ****   ")
+			
+		};
         
-        line[0] = String.join(" ","    **    ","    **    "," ******* ","   ****  ");
-        
-        line[1] = String.join(" ","  **  **  ","  **  **  "," **    * "," **      ");
-        
-        line[2] = String.join(" "," **    ** "," **    ** "," **    * ","**       ");
-        
-        line[3] = String.join(" "," **    ** "," **    ** "," ******* ","  *****   ");
-        
-        line[4] = String.join(" "," **    ** "," **    ** "," **      ","      **  ");
-        
-        line[5] = String.join(" ","  **  **  ","  **  **  "," **      "," **   **  ");
-        
-        line[6] = String.join(" ","    **    ","    **    "," **      ","   ****   ");
         
         
         for(String s: line){
