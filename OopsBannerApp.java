7c85475d24b38ad6b8c4c3fb133c7f648e872ca6
amitkr069 <aman3133@7-11.com>
2026-02-18 14:53:49 +0530

    Coded to Render OOPS as Banner using Print Statements and String.join()

---
+++ OopsBannerApp.java
@@ -0,0 +1,18 @@
+/**
+@Author Amit
+
+@Version 3.0
+
+*/
+
+class OopsBannerApp{
+	public static void main(String args[]){
+		System.out.println(String.join(" ","    **    ","    **    "," ******* ","   ****  "));
+		System.out.println(String.join(" ","  **  **  ","  **  **  "," **    * "," **      "));
+		System.out.println(String.join(" "," **    ** "," **    ** "," **    * ","**       "));
+		System.out.println(String.join(" "," **    ** "," **    ** "," ******* ","  *****   "));
+		System.out.println(String.join(" "," **    ** "," **    ** "," **      ","      **  "));
+		System.out.println(String.join(" ","  **  **  ","  **  **  "," **      "," **   **  "));
+		System.out.println(String.join(" ","    **    ","    **    "," **      ","   ****   "));
+	}
+}
