e1695f8a197189edc2fcc771f4e7d499ea74f73a
amitkr069 <aman3133@7-11.com>
2026-02-18 14:36:49 +0530

    Coded to Render OOPS as Banner using Print Statements

---
+++ OopsBannerApp.java
@@ -0,0 +1,17 @@
+/**
+@Author Amit
+@Version 2.0
+*/
+
+class OopsBannerApp{
+	public static void main(String args[]){
+		System.out.println("    **        **     *******    ****  ");
+		System.out.println("  **  **    **  **   **    *  **     ");
+		System.out.println(" **    **  **    **  **    *  **       ");
+		System.out.println(" **    **  **    **  *******   *****   ");
+		System.out.println(" **    **  **    **  **        *****  ");
+		System.out.println(" **    **  **    **  **            **        ");
+		System.out.println("  ** **     ** **    **            **       ");
+		System.out.println("    **        **     **        ****");
+	}
+}
