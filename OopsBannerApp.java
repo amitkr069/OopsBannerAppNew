b951743f85d51146a6a15a2172047461c59ff359
Amit Kumar Mandal <126327335+amitkr069@users.noreply.github.com>
2026-02-20 18:19:56 +0530

    Coded to print OOPS Banner using Methods

--- OopsBannerApp.java
+++ OopsBannerApp.java
@@ -1,12 +1,13 @@
 /**
 @Author Amit
-
 @Version 6.0
-
+This is Use Case 6.
+Here we are printing the OOPS Banner using methods.
+we are making seperate methods for each pattern and calling it in the main class.
 */
 
 // this is for printing O pattern using method
-class OopsBannerApp{
+class OopsBannerApp{ // the main class
 	public static String[] getOPattern(){
 		return new String[]{
 			"    **    ",
@@ -20,6 +21,7 @@ class OopsBannerApp{
 		
 	}
 	
+	// this is for printing P pattern using method
 	public static String[] getPPattern(){
 		return new String[]{
 			" ******* ",
@@ -32,7 +34,8 @@ class OopsBannerApp{
 		};
 	
 	}
-	
+
+	// this is for printing S pattern using method
 	public static String[] getSPattern(){
 		return new String[]{
 			"   ****  ",
@@ -45,21 +48,15 @@ class OopsBannerApp{
 		};
 	}
 	public static void main(String[] args) {
-        // System.out.println("Try programiz.pro");
         
-        
-        
-        
-        String[] OPattern = getOPattern();
+        String[] OPattern = getOPattern(); // calling the method and storing it in string array.
 		String[] PPattern = getPPattern();
 		String[] SPattern = getSPattern();
 		
 		for(int i = 0; i<OPattern.length; i++){
 			System.out.println(OPattern[i] + " " + OPattern[i] + " " + PPattern[i] + " " + SPattern[i]);
+			//calling each arrays indexes and printing the pattern row by row
 		}
-        
-        // System.out.println(line[0]);
-        
         
     }
 }
