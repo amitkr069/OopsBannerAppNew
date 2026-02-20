c884568d9dfb9bd0961538bf7647a4d28476e607
Amit Kumar Mandal <126327335+amitkr069@users.noreply.github.com>
2026-02-20 18:26:28 +0530

    Coded to render OOPS banner using Inner Class

--- OopsBannerApp.java
+++ OopsBannerApp.java
@@ -1,5 +1,12 @@
-class Banner{
-    static class GetBanners{
+/**
+@Author Amit
+@Version 7.0
+This is Use Case 6.
+Here we are printing the OOPS Banner using methods and using the concept of inner class.
+we are making seperate methods for each pattern inside the inner class and making the object of the inner class to call the methods.
+*/
+class Banner{ //outer class
+    static class GetBanners{ //inner class
         public static String[] getOPattern(){
     		return new String[]{
     			"    **    ",
@@ -43,13 +50,14 @@ class Banner{
 
 class OopsBanerApp {
     public static void main(String[] args) {
-        Banner.GetBanners banner = new Banner.GetBanners();
+        Banner.GetBanners banner = new Banner.GetBanners(); // directly creating the object of inner class as we need to access its properties. 
         
-        String[] OPattern = banner.getOPattern();
+        String[] OPattern = banner.getOPattern(); // storing the string array by calling the method of the inner class.
 		String[] PPattern = banner.getPPattern();
 		String[] SPattern = banner.getSPattern();
 		
 		for(int i = 0; i<OPattern.length; i++){
+			// printing the pattern from the arrays row by row.
 			System.out.println(OPattern[i] + " " + OPattern[i] + " " + PPattern[i] + " " + SPattern[i]);
 		}
     }
