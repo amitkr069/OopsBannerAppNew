0f5248e71e818d1f50ae532a16781fa85bd6cd58
Amit Kumar Mandal <126327335+amitkr069@users.noreply.github.com>
2026-02-20 18:02:02 +0530

    Coded to Render OOPS as Banner using Print Statements, Array and loop

--- OopsBannerApp.java
+++ OopsBannerApp.java
@@ -1,17 +1,16 @@
 /**
 @Author Amit
-
 @Version 4.0
-
+This is Use Case 4.
+In this we Print OOPS banner in star pattern using String.join and Array;
 */
 
 class OopsBannerApp{
 	public static void main(String[] args) {
-        // System.out.println("Try programiz.pro");
         
-        String line[] = new String[7];
+        String line[] = new String[7]; //initializing string array of size 7;
         
-        line[0] = String.join(" ","    **    ","    **    "," ******* ","   ****  ");
+        line[0] = String.join(" ","    **    ","    **    "," ******* ","   ****  "); .// insertring the pattern row by row using string.join
         
         line[1] = String.join(" ","  **  **  ","  **  **  "," **    * "," **      ");
         
@@ -28,6 +27,7 @@ class OopsBannerApp{
         
         for(String s: line){
             System.out.println(s);
+			//iterating each row an printing the row.
         }
         
         // System.out.println(line[0]);
