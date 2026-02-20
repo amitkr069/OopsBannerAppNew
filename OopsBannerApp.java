86d2c5114f46c7fa2b02e7a9703734711f893f71
Amit Kumar Mandal <126327335+amitkr069@users.noreply.github.com>
2026-02-20 18:12:24 +0530

    Coded to Render OOPS Banner Using Array Inline

--- OopsBannerApp.java
+++ OopsBannerApp.java
@@ -1,14 +1,16 @@
 /**
 @Author Amit
-
 @Version 5.0
 
+This is Use Case 5.
+In this we print OOPS banner in star pattern using string.join, array.
 */
 
 class OopsBannerApp{
 	public static void main(String[] args) {
-        // System.out.println("Try programiz.pro");
         
+        // Here we are inserting the pattern using Inline operation and also using string.join
+		
         String line[] = {
 			String.join(" ","    **    ","    **    "," ******* ","   ****  "),
 			String.join(" ","  **  **  ","  **  **  "," **    * "," **      "),
@@ -21,13 +23,8 @@ class OopsBannerApp{
 		};
         
         
-        
         for(String s: line){
             System.out.println(s);
         }
-        
-        // System.out.println(line[0]);
-        
-        
     }
 }
