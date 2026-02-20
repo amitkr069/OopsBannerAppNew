d1fcf2872c5b15e155d4189f0a992b3f097a98e8
Amit Kumar Mandal <126327335+amitkr069@users.noreply.github.com>
2026-02-20 11:26:37 +0530

    Coded to Render OOPS as Banner using HashMap and string builder

--- OopsBannerApp.java
+++ OopsBannerApp.java
@@ -1,56 +1,68 @@
-class Banner{
-    static class GetBanners{
-        public static String[] getOPattern(){
-    		return new String[]{
-    			"    **    ",
+/**
+@Author Amit
+@Version 2.0
+This is Use Case 8.
+this code is to print OOPS banner in star pattern HashMap and String builder.
+*/
+
+import java.util.HashMap;
+class OopsBannerApp {
+    public static HashMap<Character, String[]> createMap(){
+        HashMap<Character, String[]> banner = new HashMap<>(); //Making the HashMap object. It contains character as key and String array as value;
+        
+        banner.put('O', new String[]{
+                "    **    ",
     			"  **  **  ",
     			" **    ** ",
     			" **    ** ",
     			" **    ** ",
     			"  **  **  ",
     			"    **    "
-    		};
+        }); // in this we are putting O pattern string in map
 		
-	    }
-	    
-	    public static String[] getPPattern(){
-		return new String[]{
-			" ******* ",
-			" **    * ",
-			" **    * ",
-			" ******* ",
-			" **      ",
-			" **      ",
-			" **      "
-		};
-	
-	}
-	
-	public static String[] getSPattern(){
-		return new String[]{
-			"   ****  ",
+        
+        banner.put('P', new String[]{
+            "****** ",
+            "*     *",
+            "*     *",
+            "****** ",
+            "*      ",
+            "*      ",
+            "*      "
+        });
+		// in this we are putting P pattern string in map
+        
+        banner.put('S', new String[]{
+            "   ****  ",
 			" **      ",
 			"**       ",
 			"  *****   ",
 			"      **  ",
 			" **   **  ",
 			"   ****   "
-		};
-	}
+        });
+		
+		//// in this we are putting S pattern string in map
+        
+        return banner;
+		// returning the map(banner)
     }
     
-}
-
-class OopsBanerApp {
+    public static void printBanner(String message, HashMap<Character, String[]> banner){
+        for(int i = 0; i<7; i++){ // iterating for 7 times
+			StringBuilder sb = new StringBuilder(); // Creating the String Builder App
+            for(char c: message.toCharArray()){ //iterating through each message(oops) character by character. this will act as key
+                //System.out.print(banner.get(c)[i] + " ");
+				String[] pattern = banner.get(c); // getting the value of the key and storing it in String array "Pattern"
+				sb.append(pattern[i]).append(" "); //appending the pattern of each row line by line with space
+            }
+            
+            System.out.println(sb.toString()); //print the row and converting the string builder to string
+        }
+    }
     public static void main(String[] args) {
-        Banner.GetBanners banner = new Banner.GetBanners();
-        
-        String[] OPattern = banner.getOPattern();
-		String[] PPattern = banner.getPPattern();
-		String[] SPattern = banner.getSPattern();
-		
-		for(int i = 0; i<OPattern.length; i++){
-			System.out.println(OPattern[i] + " " + OPattern[i] + " " + PPattern[i] + " " + SPattern[i]);
-		}
+        HashMap<Character, String[]> banner = createMap(); // calling the function and stroing it in HashMap;
+        String message = "OOPS";
+        printBanner(message, banner); // calling the printBanner funciton.
     }
 }
